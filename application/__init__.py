a8350e7644a8b526cacb160278b28cf7dcd33b23
Ayush Gupta <ayush2020cs159@abesit.edu.in>
2024-04-24 10:58:29 +0530

    Update __init__.py
    replace passwd

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,8 @@ from pymongo.mongo_client import MongoClient
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecretkey"
 
-# Create a new client and connect to the server
-client = MongoClient("mongodb+srv://f8th:K3BnBtDU2eMu2Zy5@f8thdb.dese96y.mongodb.net/test?retryWrites=true&w=majority&appName=f8thDB")
+# Create a new client and connect to the server replace your own password with <password> 
+client = MongoClient("mongodb+srv://f8th:<password>@f8thdb.dese96y.mongodb.net/test?retryWrites=true&w=majority&appName=f8thDB")
 
 # Access the 'new_database' in your cluster
 db = client['test']
